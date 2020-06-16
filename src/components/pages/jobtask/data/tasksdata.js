dc95d86d6ab4a0edf4cc27093dc690699b16ad19
Kobsak <kobsak000@gmail.com>
2020-06-16 13:52:54 +0700

    g

--- src/components/pages/jobtask/data/tasksdata.js
+++ src/components/pages/jobtask/data/tasksdata.js
@@ -81,14 +81,14 @@ export default [
     jobId: 5,
     jobName: 'Finance',
     jobtask:[
-      {id:0,detail:'preparing accounts and tax returns.',criticaltask:true,knowledge:false,skill:true,attribute:false,},
-      {id:1,detail:'administering payrolls and controlling income and expenditure.',criticaltask:true,knowledge:false,skill:false,attribute:false,},
-      {id:2,detail:'auditing financial information.',criticaltask:false,knowledge:true,skill:true,attribute:true,},
-      {id:3,detail:'compiling and presenting reports, budgets, business plans, commentaries and financial statements.',criticaltask:false,knowledge:true,skill:false,attribute:true,},
-      {id:4,detail:'analysing accounts and business plans',criticaltask:true,knowledge:false,skill:true,attribute:true,},
-      {id:5,detail:'providing tax planning services with reference to current legislation',criticaltask:false,knowledge:true,skill:false,attribute:false,},
-      {id:6,detail:'financial forecasting and risk analysis',criticaltask:false,knowledge:false,skill:true,attribute:false,},
-      {id:7,detail:'dealing with insolvency cases',criticaltask:false,knowledge:false,skill:true,attribute:false,},
+      // {id:0,detail:'preparing accounts and tax returns.',criticaltask:true,knowledge:false,skill:true,attribute:false,},
+      // {id:1,detail:'administering payrolls and controlling income and expenditure.',criticaltask:true,knowledge:false,skill:false,attribute:false,},
+      // {id:2,detail:'auditing financial information.',criticaltask:false,knowledge:true,skill:true,attribute:true,},
+      // {id:3,detail:'compiling and presenting reports, budgets, business plans, commentaries and financial statements.',criticaltask:false,knowledge:true,skill:false,attribute:true,},
+      // {id:4,detail:'analysing accounts and business plans',criticaltask:true,knowledge:false,skill:true,attribute:true,},
+      // {id:5,detail:'providing tax planning services with reference to current legislation',criticaltask:false,knowledge:true,skill:false,attribute:false,},
+      // {id:6,detail:'financial forecasting and risk analysis',criticaltask:false,knowledge:false,skill:true,attribute:false,},
+      // {id:7,detail:'dealing with insolvency cases',criticaltask:false,knowledge:false,skill:true,attribute:false,},
     ]
   },
   
