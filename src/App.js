d09fce322491aa97cb7cf2bde1ddd6c15ca9a992
Kobsak <kobsak000@gmail.com>
2020-06-22 16:20:17 +0700

    gg

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 //import components
 import Header from "./components/layouts/Header";
 import Menu from "./components/layouts/Menu";
-import Certificate from './components/pages/certificate'
+import Certificate from './components/pages/certificate/certificate'
 //import materails
 import { Container } from "@material-ui/core";
 
@@ -59,7 +59,7 @@ export default function App() {
         <Menu open={open} />
         <Container className={classes.content} maxWidth={false}>
           <Switch>
-            <Route exact={true} path="/profile" component={Certificate} />
+            <Route path="/certificate" component={Certificate} />
           </Switch>
         </Container>
       </div>
